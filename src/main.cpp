918220551bae10823a93e55454a30c85413d70e0
gabrielt17 <gabriel.muniz@ufv.br>
2024-07-08 15:42:45 -0300

    Killed navigator function and implemented linear and angular speed simuntaneously

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,14 @@ DISCLAIMER: Tests made using 10-bit resolution PWM
 
 */
 
-int lastmicros = 0;
-
 #include <Arduino.h>
 #include <Motor.h>
-#include <Navigator.h>
+#include <Converter.cpp>
 #include <Controller.h>
 #include <Encoder.h>
 #include <ros.h>
 #include "Pins.h"
+#include <WiFi.h>
 
 // PWM related values
 const int freq = 5000;
@@ -25,11 +24,15 @@ const byte resolution = 10;
 const int PULSERPERROTATION = 38;
 
 // PID Controller constants
-const double kp = 0.792/38;
+const double kp = 0.792/50;
 const double kd = 0;
-const double ki = 0;
+const double ki = 0.792/150;
 
 // Global variables
+
+  // WiFi
+  const char* ssid = "NERo-Arena";
+  const char* pw = "BDPsystem10";
   
   // Time
   unsigned long previousMicros = 0;
@@ -53,9 +56,6 @@ const double ki = 0;
   // Encoder B (RIGHT)
   Encoder rencoder(encoderBChannel1, PULSERPERROTATION);
 
-  // Robot movement
-  Navigator trackbot(lcontroller, rcontroller, lmotor, rmotor, lencoder, rencoder, 0.025, 0.5);
-
 portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
 
 // Function prototypes (declarations)
@@ -63,21 +63,28 @@ void wait(int Time);
 void IRAM_ATTR lencoderCounter();
 void IRAM_ATTR rencoderCounter();
 
+geometry_msgs::Twist msg;
+motorRPM rpm;
 
 void setup() {
 
   // Initalizing serial
   Serial.begin(115200);
+//   Serial.printf(">rtarget:%3.3f\n>currentrRPM:%3.3f\n>ltarget:%3.3f\n>currentlRPM:%3.3f\n", rtarget, currentrRPM, ltarget, currentlRPM);
 
   // Attach encoder ISR's
   attachInterrupt(encoderAChannel2, lencoderCounter, RISING);
   attachInterrupt(encoderBChannel1, rencoderCounter, RISING);
+
+  msg.linear.x = 2;
+  msg.angular.z = TWO_PI;
+  rpm = convertMessage(msg);
 }
 
 void loop() {
 
-  // float ltarget = 400*cos(TWO_PI*micros()*4.5/1e6);
-  // float rtarget = -400*cos(TWO_PI*micros()*4.5/1e6);
+  float ltarget = 400*cos(TWO_PI*micros()*0.5/1e6);
+  float rtarget = -400*cos(TWO_PI*micros()*0.5/1e6);
 
   // float ltarget = 900;
   // float rtarget = -900;
@@ -99,24 +106,21 @@ void loop() {
     attachInterrupt(encoderAChannel2, lencoderCounter, RISING);
     attachInterrupt(encoderBChannel1, rencoderCounter, RISING);
   }
+  
+  float currentlRPM = lencoder.getRPM(lmotor.isClockwise);
+  float currentrRPM = rencoder.getRPM(rmotor.isClockwise);
 
-  geometry_msgs::Twist msg;
-  // msg.linear.x = 0.25*cos(M_TWOPI*micros()*4.5/10e6);
-  msg.angular.z = 30*M_PI;
-  trackbot.move(msg);
-  
-//   float currentlRPM = lencoder.getRPM(lmotor.isClockwise);
-//   float currentrRPM = rencoder.getRPM(rmotor.isClockwise);
+  float rpwm = rcontroller.controlMotor(rtarget, currentrRPM);
+  float lpwm = lcontroller.controlMotor(ltarget, currentlRPM);
 
-//   float rpwm = rcontroller.controlMotor(rtarget, currentrRPM);
-//   float lpwm = lcontroller.controlMotor(ltarget, currentlRPM);
+  // Serial.printf("rrpm:%3.3f, lrpm:%3.3f\n",rpm.rrpm, rpm.lrpm);
 
+  lmotor.setSpeed(lpwm);
+  rmotor.setSpeed(rpwm);
   
-//   rmotor.setSpeed(rpwm);
-//   lmotor.setSpeed(lpwm);
   
   
-//   Serial.printf(">rtarget:%3.3f\n>currentrRPM:%3.3f\n>ltarget:%3.3f\n>currentlRPM:%3.3f\n", rtarget, currentrRPM, ltarget, currentlRPM);
+  Serial.printf(">rtarget:%3.3f\n>currentrRPM:%3.3f\n>ltarget:%3.3f\n>currentlRPM:%3.3f\n", rtarget, currentrRPM, ltarget, currentlRPM);
 }
 
 
