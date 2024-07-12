b47d5ec15d8c8373be28deeebb2d4677b12aa332
gabrielt17 <gabriel.muniz@ufv.br>
2024-07-12 13:52:58 -0300

    License changes to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,25 @@
 /*
-
-Relevant info:
-
-DISCLAIMER: Tests made using 10-bit resolution PWM
-
-*/
+ * Tractor
+ * 
+ * Copyright 2024 Gabriel Víctor <gabriel.muniz@ufv.br>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
 
 #include <Arduino.h>
 #include <Motor.h>
